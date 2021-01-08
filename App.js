13cfd729cf6cfb1144a63773dbaa98b3f912e7b2
testUser <shubh_tiwari@hotmail.com>
2021-01-08 22:44:23 +0530

    webview addedand dependency added

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <WebView source={{ uri: 'https://blog.jscrambler.com' }} style={{ marginTop: 20 }} />
   );
 }
 
